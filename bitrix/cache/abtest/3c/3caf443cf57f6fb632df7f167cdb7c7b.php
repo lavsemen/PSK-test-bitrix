6fd60fc4a7d0afe097584394091d16095acf2ffa
lavsemen <remark.lavrentev@gmail.com>
2022-04-03 03:58:07 +0500

    clear unusable pages and folder

--- bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
+++ bitrix/cache/abtest/3c/3caf443cf57f6fb632df7f167cdb7c7b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001648939681';
-$dateexpire = '001651531681';
+$datecreate = '001648940168';
+$dateexpire = '001651532168';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";N;}';
 return true;
 ?>
